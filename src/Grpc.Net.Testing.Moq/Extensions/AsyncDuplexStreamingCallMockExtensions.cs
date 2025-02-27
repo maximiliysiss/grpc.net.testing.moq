cd8088e74d1f4e186cd3a5a72e02614a83faeb2b
makszimin <makszimin@ozon.ru>
2025-02-27 19:36:48 +0100

    add reactive handling

--- src/Grpc.Net.Testing.Moq/Extensions/AsyncDuplexStreamingCallMockExtensions.cs
+++ src/Grpc.Net.Testing.Moq/Extensions/AsyncDuplexStreamingCallMockExtensions.cs
@@ -61,4 +61,65 @@ public static class AsyncDuplexStreamingCallMockExtensions
                     getTrailersFunc: () => [],
                     disposeAction: () => { });
             });
+
+    public static IReturnsResult<T> ReturnsAsync<T, TRequest, TResponse>(
+        this IReturnsThrows<T, AsyncDuplexStreamingCall<TRequest, TResponse>> setup,
+        Func<TRequest, TResponse> func)
+        where T : class
+        => setup.Returns(
+            (Metadata? _, DateTime? _, CancellationToken _) =>
+            {
+                var (responses, ended) = (new Queue<TResponse>(), false);
+                var source = new TaskCompletionSource<bool>();
+
+                var writer = new Mock<IClientStreamWriter<TRequest>>();
+                var reader = new Mock<IAsyncStreamReader<TResponse>>();
+
+                writer
+                    .Setup(c => c.WriteAsync(It.IsAny<TRequest>()))
+                    .Callback(Handle)
+                    .Returns(Task.CompletedTask);
+                writer
+                    .Setup(c => c.CompleteAsync())
+                    .Callback(Complete)
+                    .Returns(Task.CompletedTask);
+
+                reader
+                    .Setup(c => c.MoveNext(It.IsAny<CancellationToken>()))
+                    .Returns(WaitNext);
+                reader
+                    .SetupGet(c => c.Current)
+                    .Returns(() => responses.Dequeue());
+
+                return new AsyncDuplexStreamingCall<TRequest, TResponse>(
+                    requestStream: writer.Object,
+                    responseStream: reader.Object,
+                    responseHeadersAsync: Task.FromResult(new Metadata()),
+                    getStatusFunc: () => Status.DefaultSuccess,
+                    getTrailersFunc: () => [],
+                    disposeAction: () => { });
+
+                void Handle(TRequest request)
+                {
+                    responses.Enqueue(func(request));
+                    source.TrySetResult(true);
+                }
+
+                void Complete()
+                {
+                    ended = true;
+                    source.TrySetResult(false);
+                }
+
+                async Task<bool> WaitNext()
+                {
+                    while (responses.Count is 0 && ended is false)
+                    {
+                        await source.Task;
+                        source = new TaskCompletionSource<bool>();
+                    }
+
+                    return responses.Count > 0;
+                }
+            });
 }
--- tests/Grpc.Net.Testing.Moq.Tests/Extensions/AsyncDuplexStreamingCallMockExtensionsTests.cs
+++ tests/Grpc.Net.Testing.Moq.Tests/Extensions/AsyncDuplexStreamingCallMockExtensionsTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoFixture.Xunit2;
 using FluentAssertions;
@@ -130,6 +131,109 @@ public class AsyncDuplexStreamingCallMockExtensionsTests
 
         // Assert
         messages.Should().BeEquivalentTo(expectedResponses);
+    }
+
+    [Theory, AutoData]
+    public async Task SimpleServer_ShouldReturnResponse_ByLambdaWithReactiveHandling(TestRequest[] requests)
+    {
+        // Arrange
+        var grpcMock = new Mock<TestService.TestServiceClient>();
+        grpcMock
+            .Setup(c => c.SimpleClientServerStream(null, null, default))
+            .ReturnsAsync(rs => new TestResponse { Val = rs.Val });
+
+        var client = grpcMock.Object;
+
+        // Act
+        var call = client.SimpleClientServerStream();
+
+        var messages = new List<TestResponse>(requests.Length);
+
+        var streamWriter = call.RequestStream;
+        var responseStream = call.ResponseStream;
+
+        foreach (var testRequest in requests)
+        {
+            await streamWriter.WriteAsync(testRequest);
+
+            await responseStream.MoveNext();
+            messages.Add(responseStream.Current);
+        }
+
+        await streamWriter.CompleteAsync();
+        var moveNext = await responseStream.MoveNext();
+
+        // Assert
+        moveNext.Should().BeFalse();
+
+        for (var i = 0; i < messages.Count; i++)
+            messages[i].Val.Should().Be(requests[i].Val);
+    }
+
+    [Theory, AutoData]
+    public async Task SimpleServer_ShouldReturnResponse_ByLambdaWithReactiveHandlingAndCreationTaskBeforeSendRequest(TestRequest[] requests)
+    {
+        // Arrange
+        var grpcMock = new Mock<TestService.TestServiceClient>();
+        grpcMock
+            .Setup(c => c.SimpleClientServerStream(null, null, default))
+            .ReturnsAsync(rs => new TestResponse { Val = rs.Val });
+
+        var client = grpcMock.Object;
+
+        // Act
+        var call = client.SimpleClientServerStream();
+
+        var responseTask = call.ResponseStream.ReadAllAsync().ToArrayAsync();
+        await call.RequestStream.WriteAllAsync(requests);
+
+        // Assert
+        var messages = await responseTask;
+
+        for (var i = 0; i < messages.Length; i++)
+            messages[i].Val.Should().Be(requests[i].Val);
+    }
+
+    [Theory, AutoData]
+    public async Task SimpleServer_ShouldReturnResponse_ByLambdaWithReactiveHandlingAndCallCallback(TestRequest[] requests)
+    {
+        // Arrange
+        var flag = false;
+
+        var grpcMock = new Mock<TestService.TestServiceClient>();
+        grpcMock
+            .Setup(c => c.SimpleClientServerStream(null, null, default))
+            .Callback(() => flag = true)
+            .ReturnsAsync(rs => new TestResponse { Val = rs.Val });
+
+        var client = grpcMock.Object;
+
+        // Act
+        var call = client.SimpleClientServerStream();
+
+        var messages = new List<TestResponse>(requests.Length);
+
+        var streamWriter = call.RequestStream;
+        var responseStream = call.ResponseStream;
+
+        foreach (var testRequest in requests)
+        {
+            await streamWriter.WriteAsync(testRequest);
+
+            await responseStream.MoveNext();
+            messages.Add(responseStream.Current);
+        }
+
+        await streamWriter.CompleteAsync();
+        var moveNext = await responseStream.MoveNext();
+
+        // Assert
+        moveNext.Should().BeFalse();
+
+        for (var i = 0; i < messages.Count; i++)
+            messages[i].Val.Should().Be(requests[i].Val);
+
+        flag.Should().BeTrue();
     }
 
     [Theory, AutoData]
